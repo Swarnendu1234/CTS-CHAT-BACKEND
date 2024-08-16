da22d10584e2b2930fcabed2f46ae7d124b1d70e
Swarnendu1234 <swarnendumajumdert2007@gmail.com>
2024-08-17 00:13:54 +0530

    Swarnendu Majumder

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+//connections and listerns
+const PORT = process.env.PORT || 5000;
+connectToDatabase().then(() => {
+    app.listen(PORT, () => console.log("Hey Swarnendu Server Open & Connected to Database"));
+})
+    .catch((err) => console.log(err));
+//# sourceMappingURL=index.js.map
