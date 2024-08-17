2e901ca8ebd6c5028f2d5fceb4c5ed3e81b6208e
Swarnendu1234 <159484122+Swarnendu1234@users.noreply.github.com>
2024-08-17 08:10:08 +0530

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { connectToDatabase } from "./db/connection.js";
 //connections and listerns
 const PORT = process.env.PORT || 5000;
 connectToDatabase().then(()=> {
-  app.listen(PORT, () => console.log("Hey Swarnendu Server Open & Connected to Database"))
+  app.listen(PORT, () => console.log("Hey Swarnendu Server Open & Connected to Database Hello Krishna Sir"))
 
 })
 .catch((err) => console.log(err))
